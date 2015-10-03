2e5bfe741790746348abf2f6e8a8622e42d04765
CodePretty <brigette.lundeen@gmail.com>
2015-10-03 15:07:56 -0700

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var client = new WindowsAzure.MobileServiceClient(
+    "https://issuetracker.azure-mobile.net/",
+    "OwIaGoXveiClXWOBJiCjtvhLVuEaEJ21"
+);
+
+function getIssues() {
+	console.log('soon');
+}
+
+function addIssueState() {
+	var state = {
+		Value: "New"
+	};
+	client.getTable("IssueState").insert(state);
+}
+
+function addIssue() {
+	var issue = {
+		OpenedBy: "Rich Lundeen",
+		AssignedTo: "Brigette Lundeen",
+		Description: "Something goes here",
+		Priority: 1
+	};
+	client.getTable("Issue").insert(issue);
+}
+
+getIssues();
